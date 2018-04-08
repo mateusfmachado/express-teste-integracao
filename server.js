6bcc891abbf02ebf6e4fff15d1fdf18576e6f326
mateusfmachado <mateus1198@gmail.com>
2018-04-08 14:32:38 -0300

    Adicionado alteracoes para melhoras e facilitar codigos

--- server.js
+++ server.js
@@ -1,30 +1,11 @@
-// LIBRARIES
 const express = require('express')
 const bodyParser = require('body-parser')
-var morgan = require('morgan')
-
-// CONSTANTS
 const PORT = process.env.PORT || 8085
-
-// EXEC
 const app = express()
 
-// LOGGER
-if(process.env.NODE_ENV != 'TEST') app.use(morgan('tiny'));
+app.use( bodyParser.urlencoded({ extended: false}) )
+app.use( bodyParser.json() )
 
-app.disable('x-powered-by');
-
-// METHODS SETUP
-app.use(bodyParser.urlencoded({ extended: false, limit:1024*1024*1.5 }))
-app.use(bodyParser.json({ limit:1024*1024*1.5 }))
-
-// REMOVE EXPRESS TAG FROM HEADERS
-app.use(function (req, res, next) {
-    res.removeHeader("x-powered-by");
-    next();
-});
-
-// ROUTES
 app.use('/', require('./routes'));
 
 // HANDLE NOT FOUND 404
@@ -43,9 +24,6 @@ app.use(function(err, req, res, next) {
     }});
 });  
 
-// STARTUP
-app.listen(PORT,() => {
-    if(process.env.NODE_ENV != 'TEST') console.log('Running on localhost:'+PORT)
-})
+app.listen( PORT, () => process.env.NODE_ENV != 'TEST' && console.log('Running on localhost:'+PORT) )
 
 module.exports = app
--- test/index.js
+++ test/index.js
@@ -8,27 +8,34 @@ chai.use(chaiHttp)
 describe('API', () => {
 
     it('Check Status - GET /', (done) => {
-        chai.request(app)
-        .get('/')
+
+        chai.request(app).get('/')
         .end((err, res) => {
+
             expect(res).to.have.status(200)
             expect(res.body).to.be.a('object')
+            expect(res.body.ok).to.be.an('boolean')
             expect(res.body.ok).to.be.equal(true)
             done()
+            
         })
+
     })
 
     it('Check Response - POST /', (done) => {
-        chai.request(app)
-        .post('/')
+
+        chai.request(app).post('/')
         .send({ text: "teste" })
         .end((err, res) => {
+
             expect(res).to.have.status(200)
             expect(res.body).to.be.a('object')
             expect(res.body.ok).to.be.an("string")
             expect(res.body.ok).to.be.equal("teste")
             done()
+
         })
+
     })
 
 })
